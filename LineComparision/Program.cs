e453e13c62bd39f6f8b3fdc12da9179ca9466485
charan_kanduri <charan2000kanduri@gmail.com>
2021-06-30 22:04:36 +0530

    [CHARAN] Line length compare

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -7,35 +7,43 @@ namespace LineComparision
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LineComparision Program");
-
             int x1, x2, y1, y2;
 
+            String[] lengthArray = new String[2];
 
-            Console.WriteLine("Enter X1");
-            x1 = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Enter X1");
+                x1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter Y1 ");
-            y1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Y1 ");
+                y1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter X2");
-            x2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter X2");
+                x2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter Y2 ");
-            y2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Y2 ");
+                y2 = Convert.ToInt32(Console.ReadLine());
 
-            string length = FindLength(x1, x2, y1, y2);
+                lengthArray[i] = FindLength(x1, x2, y1, y2);
+            }
+            Console.WriteLine("The lengths are" + lengthArray[0] + " " + lengthArray[1]);
 
-            Console.WriteLine("The lenght is: "+length);
-
+            if (string.Equals(lengthArray[0], lengthArray[1])) 
+            {
+                Console.WriteLine("\nBoth Lengths are same");
+            }
         }
+
 
         public static string FindLength(int a, int b, int c, int d)
         {
             double sqrt = Math.Sqrt(Math.Pow((a - b), 2) + Math.Pow((c - d), 2));
             string val = Convert.ToString(Math.Round(sqrt, 2));
             return val;
-
         }
     }
 }
+
+
 
